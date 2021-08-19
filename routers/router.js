2b77595a0766f5260c4749a4fc84bb1bd3c3267c
Dominick Simone <domsimone123@gmail.com>
2021-08-18 20:43:47 -0400

    Fixing Port on Heroku

--- routers/router.js
+++ routers/router.js
@@ -24,11 +24,7 @@ router.post("/", (req, res) => {
         newJson = JSON.stringify(data)
         console.log(newJson)
         fs.writeFileSync('./db/db.json', newJson, "utf-8")
-        const response = {
-            status: "Complete",
-            body: newNote,
-        };
-        res.json(response)
+       
     } else {
       res.json("Error in making a note. (Fill in the notes title and add note text)")   
     }
